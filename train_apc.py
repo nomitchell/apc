fb5fffe99ff6b6aa0b18cdf0de056b90a8963a1c
Noah Mitchell <noah@gorobmitchell.com>
2025-08-05 22:49:07 -0400

    'd'

--- train_apc.py
+++ train_apc.py
@@ -48,7 +48,7 @@ def main():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=32, pin_memory=True)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=16, pin_memory=True)
 
     # --- Models, Optimizer, Scheduler ---
     print("==> Building models..")
