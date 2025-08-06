bb7cd871b15bf72ece44376d7293535e5e16762e
Noah Mitchell <noah@gorobmitchell.com>
2025-08-05 21:39:25 -0400

    fix

--- train_apc.py
+++ train_apc.py
@@ -107,8 +107,8 @@ class WideBasic(nn.Module):
             )
 
     def forward(self, x):
-        out = self.dropout(self.conv1(self.relu(self.bn1(x))))
-        out = self.conv2(self.relu(self.bn2(out)))
+        out = self.dropout(self.conv1(self.relu(self.bn1(x).clone())))
+        out = self.conv2(self.relu(self.bn2(out).clone()))
         out = out + self.shortcut(x)
         return out
 
@@ -127,7 +127,7 @@ class WideResNet(nn.Module):
         self.layer1 = self._wide_layer(WideBasic, nStages[1], n, dropout_rate, stride=1)
         self.layer2 = self._wide_layer(WideBasic, nStages[2], n, dropout_rate, stride=2)
         self.layer3 = self._wide_layer(WideBasic, nStages[3], n, dropout_rate, stride=2)
-        self.bn1 = nn.BatchNorm2d(nStages[3], momentum=0.9)
+        self.bn1 = nn.Identity()
         self.relu = nn.ReLU(inplace=False)
         self.linear = nn.Linear(nStages[3], num_classes)
 
@@ -145,8 +145,8 @@ class WideResNet(nn.Module):
         out = self.conv1(x)
         out = self.layer1(out)
         out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.relu(self.bn1(out).clone())
+        out = self.layer3(out).clone()
+        out = self.relu(self.bn1(out))
         out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
